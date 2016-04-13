9c0f856a6a9efc0f957d909398b4561cbc488d50
a028391 <a028391@ismai.pt>
2016-04-13 13:07:21 +0100

    [PersonalLibrary/Base] Add "Dashboard"

--- _Build/PersonalLibrary/Base/Dashboard.xaml.cs
+++ _Build/PersonalLibrary/Base/Dashboard.xaml.cs
@@ -10,15 +10,14 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Base {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Dashboard.xaml
     /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
+    public partial class Dashboard : Window {
+        public Dashboard() {
             InitializeComponent();
         }
     }

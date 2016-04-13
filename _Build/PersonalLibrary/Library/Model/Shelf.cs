fdd93ad4e12e16d423ae8728c094fc034cb6ce0e
a028391 <a028391@ismai.pt>
2016-04-13 02:12:14 +0100

    [PersonalLibrary/../Model] Add "Shelf"

---
+++ _Build/PersonalLibrary/Library/Model/Shelf.cs
@@ -0,0 +1,11 @@
+﻿using SQLite;
+
+namespace Library.Model {
+    [Table("Shelves")]
+    public class Shelf {
+        [PrimaryKey, AutoIncrement]
+        public int slfID { get; set; }
+        public string slfName { get; set; }
+        public string slfDescription { get; set; }
+    }
+}

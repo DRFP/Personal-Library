573dfaee1d643803989b727dc556e6e26bc42b71
a028391 <a028391@ismai.pt>
2016-04-13 02:16:41 +0100

    [PersonalLibrary/Library] Add "SQLiteManager"

---
+++ _Build/PersonalLibrary/Library/SQLiteManager.cs
@@ -0,0 +1,19 @@
+﻿using Library.Model;
+using SQLite;
+using System.Threading.Tasks;
+using static Library.Configuration;
+using static Library.Database;
+
+namespace Library {
+    public class SQLiteManager {
+        private static SQLiteAsyncConnection connection;
+
+        static SQLiteManager() { connection = new SQLiteAsyncConnection(DatabaseName); }
+
+        public static async Task CreateDatabase() {
+            await connection.CreateTablesAsync<Book, Shelf>();
+            await connection.InsertAllAsync(Books());
+            await connection.InsertAllAsync(Shelves());
+        }
+    }
+}

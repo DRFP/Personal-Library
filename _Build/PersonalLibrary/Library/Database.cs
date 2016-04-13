5fc0de857aaa4a9d2b74defe1ad5672bf516def9
a028391 <a028391@ismai.pt>
2016-04-13 02:16:19 +0100

    [PersonalLibrary/Library] Add "Database"

---
+++ _Build/PersonalLibrary/Library/Database.cs
@@ -0,0 +1,37 @@
+﻿using Library.Model;
+using System;
+using System.Collections.Generic;
+
+namespace Library {
+    public class Database {
+        public static List<Book> Books() {
+            var books = new List<Book>();
+
+            books.Add(new Book {
+                slfID = 1,
+                booTitle = "Title",
+                booDescription = "Description",
+                booAuthor = "Author",
+                booPublisher = "Publisher",
+                booPublishedDate = DateTime.Now,
+                booPageCount = 100,
+                booRating = 5.0,
+                booRatingsCount = 1000,
+                booInformationURL = "Information URL",
+                booPreviewURL = "Preview URL"
+            });
+
+            return books;
+        }
+
+        public static List<Shelf> Shelves() {
+            var shelves = new List<Shelf>();
+
+            shelves.Add(new Shelf { slfName = "Reading now", slfDescription = "" });
+            shelves.Add(new Shelf { slfName = "To read", slfDescription = "" });
+            shelves.Add(new Shelf { slfName = "Have read", slfDescription = "" });
+
+            return shelves;
+        }
+    }
+}

39990724988d99157d99aa2af85a9386ae719fde
spncode <hen.spn@gmail.com>
2016-04-17 20:54:24 +0100

    [PersonalLibrary/Library] Update "Configuration"

--- _Build/PersonalLibrary/Library/Configuration.cs
+++ _Build/PersonalLibrary/Library/Configuration.cs
@@ -1,5 +1,10 @@
-﻿namespace Library {
+﻿using Google.Apis.Books.v1;
+using Google.Apis.Services;
+
+namespace Library {
     public class Configuration {
-        public const string DatabaseName = "PersonalLibrary.db";
+        public const string applicationName = "PersonalLibrary";
+        public const string apiKey = "AIzaSyCeOsGdlQrvY5KpiHlJAA9m5Fn_ANhcR0I";
+        public const string databaseName = "PersonalLibrary.db";
     }
 }

5c7c532579a0cb52b09a94316bbd8399c716cd8a
a028391 <a028391@ismai.pt>
2016-04-17 23:02:21 +0100

    [PersonalLibrary/Base] Update "UCBookInformation"

--- _Build/PersonalLibrary/Base/UCBookInformation.xaml.cs
+++ _Build/PersonalLibrary/Base/UCBookInformation.xaml.cs
@@ -1,25 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Library.Model;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using static Library.API;
 
 namespace Base {
-    /// <summary>
-    /// Interaction logic for UCBookInformation.xaml
-    /// </summary>
     public partial class UCBookInformation : UserControl {
-        public UCBookInformation() {
+        private Book book;
+
+        public UCBookInformation(Book book) {
             InitializeComponent();
+
+            Margin = new Thickness(3, 0, 0, 0);
+            this.book = book;
+
+            imgThumbnail.Source = new BitmapImage(new Uri(book.booThumbnail));
+            txbTitle.Text = book.booTitle;
+            txbDescription.Text = book.booDescription;
+            txbAuthor.Text = $"Author: {book.booAuthor}";
+            txbPublisher.Text = $"Publisher: {book.booPublisher}";
+            txbPublishedDate.Text = $"Published Date: {book.booPublishedDate}";
+            txbPageCount.Text = $"Page Count: {book.booPageCount}";
+            txbRating.Text = $"Rating: {book.booRating} ({book.booRatingsCount})";
+
+            SetShelves();
+        }
+
+        private async void SetShelves() {
+            var shelves = await GetShelves();
+            foreach (var shelf in shelves) cmbShelves.Items.Add(new ComboBoxItem() { Tag = shelf.slfID, Content = shelf.slfName });
+        }
+
+        private async void btnAdd_Click(object sender, RoutedEventArgs e) {
+            var shelf = cmbShelves.SelectedItem as ComboBoxItem;
+            if (shelf != null) await AddBook(book, int.Parse(shelf.Tag.ToString()));
         }
     }
 }

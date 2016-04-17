a7ddca3b3d6ad0d23764c57afc6b6b1d698bd2ba
a028391 <a028391@ismai.pt>
2016-04-17 23:02:56 +0100

    [PersonalLibrary/Base] Update "Dashboard"

--- _Build/PersonalLibrary/Base/Dashboard.xaml.cs
+++ _Build/PersonalLibrary/Base/Dashboard.xaml.cs
@@ -1,24 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using static Library.API;
 
 namespace Base {
-    /// <summary>
-    /// Interaction logic for Dashboard.xaml
-    /// </summary>
     public partial class Dashboard : Window {
         public Dashboard() {
             InitializeComponent();
+            LoadShelves();
         }
+
+        private async void tbxSearch_KeyDown(object sender, KeyEventArgs e) {
+            if (wplSearchedBooks.Children.Count > 0) wplSearchedBooks.Children.Clear();
+            if (e.Key == Key.Enter && tbxSearch.Text != string.Empty) {
+                var books = await GetSearchedBooks(tbxSearch.Text);
+                foreach (var book in books) {
+                    var ucBookInformation = new UCBookInformation(book);
+                    wplSearchedBooks.Children.Add(ucBookInformation);
+                }
+            }
+        }
+
+        private async void LoadShelves() {
+            var shelves = await GetShelves();
+
+            foreach (var shelf in shelves) {
+                var ucShelf = new UCShelf(shelf.slfID, shelf.slfName);
+                wplShelves.Children.Add(ucShelf);
+            }
+        }
+
+        private void imgAddShelf_MouseUp(object sender, MouseButtonEventArgs e) { grdNewShelf.Visibility = Visibility.Visible; }
+
+        private void tbxNewShelf_KeyDown(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Enter) {
+                AddShelf(tbxNewShelf.Text);
+                grdNewShelf.Visibility = Visibility.Collapsed;
+                wplShelves.Children.Clear();
+                LoadShelves();
+            }
+        }
+
+
+        private void imgMinimize_MouseUp(object sender, MouseButtonEventArgs e) { WindowState = WindowState.Minimized; }
+
+        private void imgClose_MouseUp(object sender, MouseButtonEventArgs e) { Environment.Exit(0); }
     }
 }

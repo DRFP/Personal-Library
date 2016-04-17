3d680b4201919869bda613ced936b7b92bf26b5e
a028391 <a028391@ismai.pt>
2016-04-17 23:01:57 +0100

    [PersonalLibrary/Base] Update "UCShelf"

--- _Build/PersonalLibrary/Base/UCShelf.xaml.cs
+++ _Build/PersonalLibrary/Base/UCShelf.xaml.cs
@@ -1,25 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Base {
-    /// <summary>
-    /// Interaction logic for UCShelf.xaml
-    /// </summary>
     public partial class UCShelf : UserControl {
-        public UCShelf() {
+        public int id;
+
+        public UCShelf(int id, string name) {
             InitializeComponent();
+
+            Margin = new Thickness(0, 0, 0, 6);
+
+            this.id = id;
+            tbxName.Text = name;
         }
     }
 }

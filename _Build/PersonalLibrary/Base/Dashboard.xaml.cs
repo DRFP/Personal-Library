f91e8d122b675e10a060f2a76d484e9f42351a11
a028391 <a028391@ismai.pt>
2016-05-15 17:02:03 +0100

    [PersonalLibrary/Base] Update "Dashboard"

--- _Build/PersonalLibrary/Base/Dashboard.xaml.cs
+++ _Build/PersonalLibrary/Base/Dashboard.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
 using static Library.API;
 
 namespace Base {
     public partial class Dashboard : Window {
+        private bool editShelf, removeShelf, removeBook;
         private int shelfID;
 
         public Dashboard() {
@@ -79,6 +82,35 @@ namespace Base {
             removeShelf = true;
         }
 
+        private void ucBook_onClick(UCBook ucBook) {
+            imgThumbnail.Source = new BitmapImage(new Uri(ucBook.book.booThumbnail));
+
+            txbTitle.Text = ucBook.book.booTitle;
+            txbDescription.Text = ucBook.book.booDescription;
+            txbAuthor.Text = $"Author: {ucBook.book.booAuthor}";
+            txbPublisher.Text = $"Publisher: {ucBook.book.booPublisher}";
+            txbPublishedDate.Text = $"Published Date: {ucBook.book.booPublishedDate}";
+            txbPageCount.Text = $"Page Count: {ucBook.book.booPageCount}";
+            txbRating.Text = $"Rating: {ucBook.book.booRating} ({ucBook.book.booRatingsCount})";
+
+            if (removeBook) ((Storyboard)Resources["MoveToRight"]).Begin();
+            else ((Storyboard)Resources["MoveToLeft"]).Begin();
+
+            removeBook = false;
+        }
+
+        private void ucBook_onClickRemove(UCBook ucBook) {
+            RemoveBook(ucBook.book);
+            wplShelfBooks.Children.Remove(ucBook);
+            removeBook = true;
+        }
+
+        private void imgAddShelf_MouseUp(object sender, MouseButtonEventArgs e) {
+            grdNewShelf.Visibility = Visibility.Visible;
+            txbNewShelf.Text = "New Shelf";
+            tbxNewShelf.Text = "Write here the new shelf name...";
+            ((Storyboard)Resources["MoveToUp"]).Begin();
+        }
 
         private void tbxNewShelf_KeyDown(object sender, KeyEventArgs e) {
             if (e.Key == Key.Enter) {
@@ -90,6 +122,13 @@ namespace Base {
             }
         }
 
+        private void grdBase_KeyDown(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Escape) {
+                if (grdNewShelf.Margin == new Thickness(14, 552, 0, 0)) ((Storyboard)Resources["MoveToBottom"]).Begin();
+                if (grdBookInformation.Margin == new Thickness(706, 28, 0, 0)) ((Storyboard)Resources["MoveToRight"]).Begin();
+            }
+            else if (e.Key == Key.Enter) LoadShelves();
+        }
 
         private void imgMinimize_MouseUp(object sender, MouseButtonEventArgs e) { WindowState = WindowState.Minimized; }
 

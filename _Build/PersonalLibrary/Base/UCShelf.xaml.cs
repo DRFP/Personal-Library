947788d7c04c567682eca6a9c707a08c6a63dbe7
spncode <hen.spn@gmail.com>
2016-05-15 16:58:25 +0100

    [PersonalLibrary/Base] Update "UCShelf"

--- _Build/PersonalLibrary/Base/UCShelf.xaml.cs
+++ _Build/PersonalLibrary/Base/UCShelf.xaml.cs
@@ -1,8 +1,13 @@
-﻿using System.Windows;
+﻿using Library.Model;
+using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Base {
     public partial class UCShelf : UserControl {
+        public event Action<UCShelf> onClick, onClickEdit, onClickRemove;
+        public Shelf shelf;
         public int id;
 
         public UCShelf(int id, string name) {
@@ -12,6 +17,17 @@ namespace Base {
 
             this.id = id;
             tbxName.Text = name;
+
+            if (tbxName.Text == "Reading now" || tbxName.Text == "To read" || tbxName.Text == "Have read") {
+                imgEdit.Visibility = Visibility.Hidden;
+                imgRemove.Visibility = Visibility.Hidden;
+            }
         }
+
+        private void grdBase_MouseUp(object sender, MouseButtonEventArgs e) { onClick?.Invoke(this); }
+
+        private void imgEdit_MouseUp(object sender, MouseButtonEventArgs e) { onClickEdit?.Invoke(this); }
+
+        private void imgRemove_MouseUp(object sender, MouseButtonEventArgs e) { onClickRemove?.Invoke(this); }
     }
 }

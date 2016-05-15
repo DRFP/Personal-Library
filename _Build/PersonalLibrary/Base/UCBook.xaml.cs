b0f6b7f2701efa04b7df6c0ca5a1d1259832a725
a028391 <a028391@ismai.pt>
2016-05-15 17:02:22 +0100

    [PersonalLibrary/Base] Update "UCBook"

--- _Build/PersonalLibrary/Base/UCBook.xaml.cs
+++ _Build/PersonalLibrary/Base/UCBook.xaml.cs
@@ -1,9 +1,25 @@
-﻿using System.Windows.Controls;
+﻿using Library.Model;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace Base {
     public partial class UCBook : UserControl {
-        public UCBook() {
+        public event Action<UCBook> onClick, onClickRemove;
+        public Book book;
+
+        public UCBook(Book book) {
             InitializeComponent();
+            Margin = new Thickness(5, 5, 5, 5);
+
+            this.book = book;
+            imgThumbnail.Source = new BitmapImage(new Uri(book.booThumbnail));
         }
+
+        private void grdBase_MouseUp(object sender, MouseButtonEventArgs e) { onClick?.Invoke(this); }
+
+        private void imgRemove_MouseUp(object sender, MouseButtonEventArgs e) { onClickRemove?.Invoke(this); }
     }
 }

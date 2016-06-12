2612bec72772b146604a8de5b36d871a0b41b08e
a028391 <a028391@ismai.pt>
2016-06-12 19:03:14 +0100

    [PersonalLibrary/Library] Update "Database"

--- _Build/PersonalLibrary/Library/Database.cs
+++ _Build/PersonalLibrary/Library/Database.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 
 namespace Library {
-    public class Database {
-        public static List<Shelf> Shelves() {
-            var shelves = new List<Shelf>();
+	public class Database {
+		public static List<Shelf> Shelves() {
+			var shelves = new List<Shelf>();
 
-            shelves.Add(new Shelf { slfName = "Reading now" });
-            shelves.Add(new Shelf { slfName = "To read" });
-            shelves.Add(new Shelf { slfName = "Have read" });
+			shelves.Add(new Shelf { slfName = "Reading now" });
+			shelves.Add(new Shelf { slfName = "To read" });
+			shelves.Add(new Shelf { slfName = "Have read" });
 
-            return shelves;
-        }
-    }
+			return shelves;
+		}
+	}
 }

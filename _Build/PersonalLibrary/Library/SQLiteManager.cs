160dcb0c83001594d76daf1235401c8e5ea85d06
a028391 <a028391@ismai.pt>
2016-06-12 19:02:09 +0100

    [PersonalLibrary/Library] Update "SQLite*"

--- _Build/PersonalLibrary/Library/SQLiteManager.cs
+++ _Build/PersonalLibrary/Library/SQLiteManager.cs
@@ -5,14 +5,14 @@ using static Library.Configuration;
 using static Library.Database;
 
 namespace Library {
-    public class SQLiteManager {
-        private static SQLiteAsyncConnection connection;
+	public class SQLiteManager {
+		private static SQLiteAsyncConnection connection;
 
-        static SQLiteManager() { connection = new SQLiteAsyncConnection(databaseName); }
+		static SQLiteManager() { connection = new SQLiteAsyncConnection(databaseName); }
 
-        public static async Task CreateDatabase() {
-            await connection.CreateTablesAsync<Book, Shelf>();
-            await connection.InsertAllAsync(Shelves());
-        }
-    }
+		public static async Task CreateDatabase() {
+			await connection.CreateTablesAsync<Book, Borrowing, Shelf>();
+			await connection.InsertAllAsync(Shelves());
+		}
+	}
 }

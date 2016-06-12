680589b572aa9f6c7511e792d695a2b2f4a0afce
a028391 <a028391@ismai.pt>
2016-06-12 19:03:28 +0100

    [PersonalLibrary/Library] Update "Configuration"

--- _Build/PersonalLibrary/Library/Configuration.cs
+++ _Build/PersonalLibrary/Library/Configuration.cs
@@ -1,7 +1,7 @@
 ﻿namespace Library {
-    public class Configuration {
-        public const string applicationName = "PersonalLibrary";
-        public const string apiKey = "AIzaSyCeOsGdlQrvY5KpiHlJAA9m5Fn_ANhcR0I";
-        public const string databaseName = "PersonalLibrary.db";
-    }
+	public class Configuration {
+		public const string applicationName = "PersonalLibrary";
+		public const string apiKey = "AIzaSyCeOsGdlQrvY5KpiHlJAA9m5Fn_ANhcR0I";
+		public const string databaseName = "PersonalLibrary.db";
+	}
 }

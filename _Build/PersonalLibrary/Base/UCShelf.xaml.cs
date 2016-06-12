0a1e10d038c11824d4153defa4012a6718f11a37
spncode <hen.spn@gmail.com>
2016-06-12 18:54:09 +0100

    [PersonalLibrary/Base] Update "UCShelf"

--- _Build/PersonalLibrary/Base/UCShelf.xaml.cs
+++ _Build/PersonalLibrary/Base/UCShelf.xaml.cs
@@ -5,29 +5,35 @@ using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace Base {
-    public partial class UCShelf : UserControl {
-        public event Action<UCShelf> onClick, onClickEdit, onClickRemove;
-        public Shelf shelf;
-        public int id;
+	public partial class UCShelf : UserControl {
+		public event Action<UCShelf> onClick, onClickEdit, onClickRemove;
+		public Shelf shelf;
+		public int id;
 
-        public UCShelf(int id, string name) {
-            InitializeComponent();
+		public UCShelf(int id, string name) {
+			InitializeComponent();
 
-            Margin = new Thickness(0, 0, 0, 6);
+			Margin = new Thickness(0, 0, 0, 6);
 
-            this.id = id;
-            tbxName.Text = name;
+			this.id = id;
+			tbxName.Text = name;
 
-            if (tbxName.Text == "Reading now" || tbxName.Text == "To read" || tbxName.Text == "Have read") {
-                imgEdit.Visibility = Visibility.Hidden;
-                imgRemove.Visibility = Visibility.Hidden;
-            }
-        }
+			if (tbxName.Text == "Reading now" || tbxName.Text == "To read" || tbxName.Text == "Have read") {
+				imgEdit.Visibility = Visibility.Hidden;
+				imgRemove.Visibility = Visibility.Hidden;
+			}
+		}
 
-        private void grdBase_MouseUp(object sender, MouseButtonEventArgs e) { onClick?.Invoke(this); }
+		private void grdBase_MouseUp(object sender, MouseButtonEventArgs e) {
+			onClick?.Invoke(this);
+		}
 
-        private void imgEdit_MouseUp(object sender, MouseButtonEventArgs e) { onClickEdit?.Invoke(this); }
+		private void imgEdit_MouseUp(object sender, MouseButtonEventArgs e) {
+			onClickEdit?.Invoke(this);
+		}
 
-        private void imgRemove_MouseUp(object sender, MouseButtonEventArgs e) { onClickRemove?.Invoke(this); }
-    }
+		private void imgRemove_MouseUp(object sender, MouseButtonEventArgs e) {
+			onClickRemove?.Invoke(this);
+		}
+	}
 }

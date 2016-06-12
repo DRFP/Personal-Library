0e995a08b561421294cc39d925a68582030cd905
spncode <hen.spn@gmail.com>
2016-06-12 18:49:11 +0100

    [PersonalLibrary/../Model] Add "Borrowing"

---
+++ _Build/PersonalLibrary/Library/Model/Borrowing.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+
+namespace Library.Model {
+	[Table("Borrowings")]
+	public class Borrowing {
+		[PrimaryKey, AutoIncrement]
+		public int borID { get; set; }
+		public int booID { get; set; }
+		public string borName { get; set; }
+		public DateTime borDeliverydDate { get; set; }
+		public string borObservations { get; set; }
+		public bool borBorrowed { get; set; }
+	}
+}

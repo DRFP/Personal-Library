3847b9b54b6f5826288d468d844c1579360e019e
a028391 <a028391@ismai.pt>
2016-06-12 19:02:43 +0100

    [PersonalLibrary/../Model] Update "Book"

--- _Build/PersonalLibrary/Library/Model/Book.cs
+++ _Build/PersonalLibrary/Library/Model/Book.cs
@@ -1,21 +1,23 @@
 ﻿using SQLite;
 
 namespace Library.Model {
-    [Table("Books")]
-    public class Book {
-        [PrimaryKey]
-        public long booISBN { get; set; }
-        public int slfID { get; set; }
-        public string booTitle { get; set; }
-        public string booDescription { get; set; }
-        public string booAuthor { get; set; }
-        public string booPublisher { get; set; }
-        public string booPublishedDate { get; set; }
-        public int booPageCount { get; set; }
-        public double booRating { get; set; }
-        public int booRatingsCount { get; set; }
-        public string booInformationLink { get; set; }
-        public string booPreviewLink { get; set; }
-        public string booThumbnail { get; set; }
-    }
+	[Table("Books")]
+	public class Book {
+		[PrimaryKey, AutoIncrement]
+		public int booID { get; set; }
+		public long booISBN { get; set; }
+		public int slfID { get; set; }
+		public string booTitle { get; set; }
+		public string booDescription { get; set; }
+		public string booCategory { get; set; }
+		public string booAuthor { get; set; }
+		public string booPublisher { get; set; }
+		public string booPublishedDate { get; set; }
+		public int booPageCount { get; set; }
+		public int booRating { get; set; }
+		public string booInformationLink { get; set; }
+		public string booPreviewLink { get; set; }
+		public string booThumbnail { get; set; }
+		public string booNotes { get; set; }
+	}
 }
